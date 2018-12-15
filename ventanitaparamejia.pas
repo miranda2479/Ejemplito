1346dea05b561ce75f64ab58cb47559756fde70a
miranda2479 <miranda434castellanos@gmail.com>
2018-12-15 13:49:07 -0700

    feat: prueba que funcione

---
+++ ventanitaparamejia.pas
@@ -0,0 +1,27 @@
+unit ventanitaparamejia;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, Buttons, StdCtrls, Mask, ExtCtrls;
+
+type
+  TForm3 = class(TForm)
+    Image1: TImage;
+    MaskEdit1: TMaskEdit;
+    SpeedButton1: TSpeedButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.dfm}
+
+end.
